03e67e10a2b20bbfe2a9932cb756c071b8310ca9
UNOSHE0516 <hiro0516jms@gmail.com>
2024-11-14 20:46:31 +0900

    コンストラクタ練習

--- src/Test.java
+++ src/Test.java
@@ -12,5 +12,18 @@ public class Test {
 		Person jiro = new Person("jiro",20);
 		System.out.println(jiro.name);
 		System.out.println(jiro.age);
+		
+		
+		Person a =new Person("saburo");
+		System.out.println(a.name);
+		System.out.println(a.age);
+		
+		Person b = new Person(25);
+		System.out.println(b.name);
+		System.out.println(b.age);
+		
+		Person c = new Person("hanako",17);
+		System.out.println(c.name);
+		System.out.println(c.age);
 	}
 }
